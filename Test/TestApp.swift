4ae9049292c2f06caca60f19271d7b97d6a0e04b
SIARHEI DUBKO <dolph-1@yandex.ru>
2022-01-06 14:30:29 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Siarhei Dubko on 6.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
